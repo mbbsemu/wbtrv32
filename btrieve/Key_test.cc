a806f5d0c2d4987931341bd392874d9e15b8763c
Todd Jeffreys <todd@voidpointer.org>
2024-05-28 16:46:11 -0600

    Add more integer conversion tests

--- btrieve/Key_test.cc
+++ btrieve/Key_test.cc
@@ -24,6 +24,7 @@ TEST(Key, SingleKeySegment) {
 }
 
 struct ParameterizedFixtureType {
+  const unsigned char *data;
   int length;
   KeyDataType type;
   uint64_t expected;
@@ -32,7 +33,7 @@ struct ParameterizedFixtureType {
 class ParameterizedFixture
     : public testing::TestWithParam<ParameterizedFixtureType> {};
 
-TEST_P(ParameterizedFixture, TestNegativeIntegerConversions) {
+TEST_P(ParameterizedFixture, NegativeIntegerTypeConversions) {
   const ParameterizedFixtureType &view = GetParam();
 
   KeyDefinition keyDefinition(0, view.length, 0, view.type, UseExtendedDataType,
@@ -42,12 +43,7 @@ TEST_P(ParameterizedFixture, TestNegativeIntegerConversions) {
 
   auto actual = static_cast<uint64_t>(
       key.keyDataToSqliteObject(
-             std::basic_string_view<uint8_t>(DATA_NEGATIVE, view.length))
-          .getIntegerValue());
-
-  actual = static_cast<uint64_t>(
-      key.keyDataToSqliteObject(
-             std::basic_string_view<uint8_t>(DATA_NEGATIVE, view.length))
+             std::basic_string_view<uint8_t>(view.data, view.length))
           .getIntegerValue());
 
   EXPECT_EQ(actual, view.expected);
@@ -55,26 +51,52 @@ TEST_P(ParameterizedFixture, TestNegativeIntegerConversions) {
 
 static std::vector<ParameterizedFixtureType> create() {
   return std::vector<ParameterizedFixtureType>{
-      {2, KeyDataType::Integer, static_cast<uint64_t>(-3343)},
-      {4, KeyDataType::Integer, static_cast<uint64_t>(-185339151)},
-      {6, KeyDataType::Integer, static_cast<uint64_t>(-9938739662095l)},
-      {8, KeyDataType::Integer, static_cast<uint64_t>(-506664896818842895)},
-      {2, KeyDataType::AutoInc, static_cast<uint64_t>(-3343)},
-      {4, KeyDataType::AutoInc, static_cast<uint64_t>(-185339151)},
-      {6, KeyDataType::AutoInc, static_cast<uint64_t>(-9938739662095)},
-      {8, KeyDataType::AutoInc, static_cast<uint64_t>(-506664896818842895)},
-      {2, KeyDataType::Unsigned, 0xF2F1},
-      {4, KeyDataType::Unsigned, 0xF4F3F2F1},
-      {6, KeyDataType::Unsigned, 0xF6F5F4F3F2F1},
-      {8, KeyDataType::Unsigned, 0xF8F7F6F5F4F3F2F1},
-      {2, KeyDataType::UnsignedBinary, 0xF2F1},
-      {4, KeyDataType::UnsignedBinary, 0xF4F3F2F1},
-      {6, KeyDataType::UnsignedBinary, 0xF6F5F4F3F2F1},
-      {8, KeyDataType::UnsignedBinary, 0xF8F7F6F5F4F3F2F1},
-      {2, KeyDataType::OldBinary, 0xF2F1},
-      {4, KeyDataType::OldBinary, 0xF4F3F2F1},
-      {6, KeyDataType::OldBinary, 0xF6F5F4F3F2F1},
-      {8, KeyDataType::OldBinary, 0xF8F7F6F5F4F3F2F1}};
+      {DATA_NEGATIVE, 2, KeyDataType::Integer, static_cast<uint64_t>(-3343)},
+      {DATA_NEGATIVE, 4, KeyDataType::Integer,
+       static_cast<uint64_t>(-185339151)},
+      {DATA_NEGATIVE, 6, KeyDataType::Integer,
+       static_cast<uint64_t>(-9938739662095l)},
+      {DATA_NEGATIVE, 8, KeyDataType::Integer,
+       static_cast<uint64_t>(-506664896818842895)},
+      {DATA_NEGATIVE, 2, KeyDataType::AutoInc, static_cast<uint64_t>(-3343)},
+      {DATA_NEGATIVE, 4, KeyDataType::AutoInc,
+       static_cast<uint64_t>(-185339151)},
+      {DATA_NEGATIVE, 6, KeyDataType::AutoInc,
+       static_cast<uint64_t>(-9938739662095)},
+      {DATA_NEGATIVE, 8, KeyDataType::AutoInc,
+       static_cast<uint64_t>(-506664896818842895)},
+      {DATA_NEGATIVE, 2, KeyDataType::Unsigned, 0xF2F1},
+      {DATA_NEGATIVE, 4, KeyDataType::Unsigned, 0xF4F3F2F1},
+      {DATA_NEGATIVE, 6, KeyDataType::Unsigned, 0xF6F5F4F3F2F1},
+      {DATA_NEGATIVE, 8, KeyDataType::Unsigned, 0xF8F7F6F5F4F3F2F1},
+      {DATA_NEGATIVE, 2, KeyDataType::UnsignedBinary, 0xF2F1},
+      {DATA_NEGATIVE, 4, KeyDataType::UnsignedBinary, 0xF4F3F2F1},
+      {DATA_NEGATIVE, 6, KeyDataType::UnsignedBinary, 0xF6F5F4F3F2F1},
+      {DATA_NEGATIVE, 8, KeyDataType::UnsignedBinary, 0xF8F7F6F5F4F3F2F1},
+      {DATA_NEGATIVE, 2, KeyDataType::OldBinary, 0xF2F1},
+      {DATA_NEGATIVE, 4, KeyDataType::OldBinary, 0xF4F3F2F1},
+      {DATA_NEGATIVE, 6, KeyDataType::OldBinary, 0xF6F5F4F3F2F1},
+      {DATA_NEGATIVE, 8, KeyDataType::OldBinary, 0xF8F7F6F5F4F3F2F1},
+      {DATA_POSITIVE, 2, KeyDataType::Integer, 0x201},
+      {DATA_POSITIVE, 4, KeyDataType::Integer, 0x4030201},
+      {DATA_POSITIVE, 6, KeyDataType::Integer, 0x60504030201},
+      {DATA_POSITIVE, 8, KeyDataType::Integer, 0x807060504030201},
+      {DATA_POSITIVE, 2, KeyDataType::AutoInc, 0x201},
+      {DATA_POSITIVE, 4, KeyDataType::AutoInc, 0x4030201},
+      {DATA_POSITIVE, 6, KeyDataType::AutoInc, 0x60504030201},
+      {DATA_POSITIVE, 8, KeyDataType::AutoInc, 0x807060504030201},
+      {DATA_POSITIVE, 2, KeyDataType::UnsignedBinary, 0x201},
+      {DATA_POSITIVE, 4, KeyDataType::UnsignedBinary, 0x4030201},
+      {DATA_POSITIVE, 6, KeyDataType::UnsignedBinary, 0x60504030201},
+      {DATA_POSITIVE, 8, KeyDataType::UnsignedBinary, 0x807060504030201},
+      {DATA_POSITIVE, 2, KeyDataType::Unsigned, 0x201},
+      {DATA_POSITIVE, 4, KeyDataType::Unsigned, 0x4030201},
+      {DATA_POSITIVE, 6, KeyDataType::Unsigned, 0x60504030201},
+      {DATA_POSITIVE, 8, KeyDataType::Unsigned, 0x807060504030201},
+      {DATA_POSITIVE, 2, KeyDataType::OldBinary, 0x201},
+      {DATA_POSITIVE, 4, KeyDataType::OldBinary, 0x4030201},
+      {DATA_POSITIVE, 6, KeyDataType::OldBinary, 0x60504030201},
+      {DATA_POSITIVE, 8, KeyDataType::OldBinary, 0x807060504030201}};
 }
 
 INSTANTIATE_TEST_CASE_P(Key, ParameterizedFixture,
